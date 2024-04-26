922174c5e52e54988a4078fb67f051256380a104
Prabhat <krsinghp8@gmail.com>
2024-04-26 06:24:22 -0400

    03

--- src/App.java
+++ src/App.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 public class App {
 
-  public static void selectionSort(int[] arr) {
-    int mi;
-    for (int i = 0; i < arr.length - 1; i++) {
-      mi = i;
-      for (int j = i + 1; j < arr.length; j++) {
-        if (isSmaller(arr, j, mi)) {
-          mi = j;
+  public static void insertionSort(int[] arr) {
+    for (int i = 1; i <= arr.length - 1; i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (isGreater(arr, j, j + 1)) {
+          swap(arr, j, j + 1);
+        } else {
+          break;
         }
       }
-      swap(arr, i, mi);
+    }
+    for (int val : arr) {
+      System.out.print(val + " ");
     }
   }
 
@@ -21,12 +23,12 @@ public class App {
     arr[j] = temp;
   }
 
-  public static boolean isSmaller(int[] arr, int i, int j) {
+  public static boolean isGreater(int[] arr, int j, int i) {
     if (arr[i] < arr[j]) return true; else return false;
   }
 
   public static void main(String[] args) {
-    int[] arr = { 5, 9, 8, 2, 1 };
-    selectionSort(arr);
+    int[] arr = { 2, 9, 5, 1, 3 };
+    insertionSort(arr);
   }
 }

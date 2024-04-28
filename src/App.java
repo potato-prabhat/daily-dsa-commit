a49e9905428cfa115875bece12a25e888a2efe1b
Prabhat <krsinghp8@gmail.com>
2024-04-28 02:33:19 -0400

    max sum non adjacent elements

--- src/App.java
+++ src/App.java
@@ -2,33 +2,17 @@ import java.util.*;
 
 public class App {
 
-  public static void insertionSort(int[] arr) {
-    for (int i = 1; i <= arr.length - 1; i++) {
-      for (int j = i - 1; j >= 0; j--) {
-        if (isGreater(arr, j, j + 1)) {
-          swap(arr, j, j + 1);
-        } else {
-          break;
-        }
-      }
-    }
-    for (int val : arr) {
-      System.out.print(val + " ");
-    }
-  }
-
-  public static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-
-  public static boolean isGreater(int[] arr, int j, int i) {
-    if (arr[i] < arr[j]) return true; else return false;
-  }
-
   public static void main(String[] args) {
-    int[] arr = { 2, 9, 5, 1, 3 };
-    insertionSort(arr);
+    int[] arr = { 5, 10, 10, 100, 5, 6 };
+    int oldi, newi, olde, newe;
+    oldi = arr[0];
+    olde = 0;
+    for (int i = 1; i < arr.length; i++) {
+      newi = arr[i] + olde;
+      newe = Math.max(oldi, olde);
+      olde = newe;
+      oldi = newi;
+    }
+    System.out.println(Math.max(oldi, oldi));
   }
 }

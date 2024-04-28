9c694db2f3fddb8186cf5f2dde8386fecaf16269
Prabhat <krsinghp8@gmail.com>
2024-04-28 13:35:32 -0400

    Paint house 2

--- src/App.java
+++ src/App.java
@@ -10,12 +10,43 @@ public class App {
       { 1, 2, 3, 9, 1, 7 },
     };
     int dp[][] = new int[hc.length][hc[0].length];
-    for (int i = 0; i < hc.length; i++) {
-      for (int j = 0; j < hc.length; j++) {
-        if (i == 0) {
-          dp[i][j] = hc[i][j];
-        }
+    int min1 = Integer.MAX_VALUE;
+    int min2 = Integer.MAX_VALUE;
+    for (int j = 0; j < hc[0].length; j++) {
+      dp[0][j] = hc[0][j];
+      if (hc[0][j] <= min1) {
+        min2 = min1;
+        min1 = hc[0][j];
+      } else if (hc[0][j] <= min2) {
+        min2 = hc[0][j];
       }
     }
+
+    for (int i = 1; i < hc.length; i++) { // i is house number
+      int tmin1 = Integer.MAX_VALUE;
+      int tmin2 = Integer.MAX_VALUE;
+      for (int j = 0; j < hc[0].length; j++) { // j is color number
+        if (hc[i - 1][j] == min1) {
+          dp[i][j] = hc[i][j] + min2;
+        } else {
+          dp[i][j] = hc[i][j] + min1;
+        }
+        if (dp[i][j] <= tmin1) {
+          tmin2 = tmin1;
+          tmin1 = dp[i][j];
+        } else if (dp[i][j] <= tmin2) {
+          tmin2 = dp[i][j];
+        }
+      }
+      min1 = tmin1;
+      min2 = tmin2;
+    }
+    int min = Integer.MAX_VALUE;
+    for (int k = 0; k < dp[0].length; k++) {
+      if (dp[dp.length - 1][k] < min) {
+        min = dp[dp.length - 1][k];
+      }
+    }
+    System.out.println(min);
   }
 }

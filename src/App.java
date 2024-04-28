e6e2cc8076a4ec7572f32ea09c30a0df0e44d0e4
Prabhat <krsinghp8@gmail.com>
2024-04-28 13:04:24 -0400

    Paint house 2

--- src/App.java
+++ src/App.java
@@ -3,16 +3,19 @@ import java.util.*;
 public class App {
 
   public static void main(String[] args) {
-    int[] arr = { 5, 10, 10, 100, 5, 6 };
-    int oldi, newi, olde, newe;
-    oldi = arr[0];
-    olde = 0;
-    for (int i = 1; i < arr.length; i++) {
-      newi = arr[i] + olde;
-      newe = Math.max(oldi, olde);
-      olde = newe;
-      oldi = newi;
+    int[][] hc = {
+      { 1, 5, 7, 2, 1, 4 },
+      { 5, 8, 4, 3, 6, 1 },
+      { 3, 2, 9, 7, 2, 3 },
+      { 1, 2, 3, 9, 1, 7 },
+    };
+    int dp[][] = new int[hc.length][hc[0].length];
+    for (int i = 0; i < hc.length; i++) {
+      for (int j = 0; j < hc.length; j++) {
+        if (i == 0) {
+          dp[i][j] = hc[i][j];
+        }
+      }
     }
-    System.out.println(Math.max(oldi, oldi));
   }
 }

8403ec26d39d42d93e2376e842b8fc21f491312e
Prabhat <krsinghp8@gmail.com>
2024-05-01 10:19:44 -0400

    target sum pair print

--- src/App.java
+++ src/App.java
@@ -3,23 +3,65 @@ import javax.lang.model.util.Elements;
 
 public class App {
 
-  public static void main(String[] args) {
-    int n = 5, k = 3;
-    int[] l2same = new int[n + 1];
-    int[] l2diff = new int[n + 1];
-    int[] tot = new int[n + 1];
-
-    l2same[2] = k;
-    l2diff[2] = k * (k - 1);
-    tot[2] = l2same[2] + l2diff[2];
-
-    for (int i = 3; i < tot.length; i++) {
-      l2same[i] = l2diff[i - 1];
-      l2diff[i] = tot[i - 1] * (k - 1);
-      tot[i] = l2same[i] + l2diff[i];
+  public static int[] mergeSort(int[] arr, int lo, int hi) {
+    if (lo == hi) {
+      int[] ba = new int[1];
+      ba[0] = arr[lo];
+      return ba;
     }
 
-    int ways = tot[n];
-    System.out.println(ways);
+    int mid = (lo + hi) / 2;
+    int[] fsh = mergeSort(arr, lo, mid);
+    int[] lsh = mergeSort(arr, mid + 1, hi);
+    int[] fsa = mergeSortedArray(fsh, lsh);
+    return fsa;
+  }
+
+  public static int[] mergeSortedArray(int[] arr1, int[] arr2) {
+    int[] arr3 = new int[arr1.length + arr2.length];
+    int i = 0, j = 0, k = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        arr3[k] = arr1[i];
+        i++;
+      } else {
+        arr3[k] = arr2[j];
+        j++;
+      }
+      k++;
+    }
+    while (i < arr1.length) {
+      arr3[k] = arr1[i];
+      i++;
+      k++;
+    }
+    while (j < arr2.length) {
+      arr3[k] = arr2[j];
+      j++;
+      k++;
+    }
+    return arr3;
+  }
+
+  public static void targetSumPairPrint(int[] arr, int tar) {
+    int[] sortedArr = mergeSort(arr, 0, arr.length - 1);
+    int l = 0, r = sortedArr.length - 1;
+    while (l < r) {
+      if (sortedArr[l] + sortedArr[r] > tar) {
+        r--;
+      } else if (sortedArr[l] + sortedArr[r] < tar) {
+        l++;
+      } else {
+        System.out.println(sortedArr[l] + " - " + sortedArr[r]);
+        r--;
+        l++;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 7, 15, 3, 18, 6, 4, 19, 2, 12, 11, 9 };
+    targetSumPairPrint(arr, 15);
   }
 }

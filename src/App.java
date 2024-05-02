72fda830b055efa1531c2fc1654c8f2baba66462
Prabhat <krsinghp8@gmail.com>
2024-05-02 12:20:05 -0400

    mxn floor tiling

--- src/App.java
+++ src/App.java
@@ -4,12 +4,19 @@ import javax.lang.model.util.Elements;
 public class App {
 
   public static void main(String[] args) {
-    int n = 6;
+    int m = 3;
+    int n = 8;
+
     int[] dp = new int[n + 1];
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2];
+
+    for (int i = 1; i <= n; i++) {
+      if (i < m) {
+        dp[i] = 1;
+      } else if (i == m) {
+        dp[i] = 2;
+      } else {
+        dp[i] = dp[i - 1] + dp[i - m];
+      }
     }
     System.out.println(dp[n]);
   }

d7a1aa6189c9128eb7660a80d95515c987148733
Prabhat <krsinghp8@gmail.com>
2024-05-02 12:57:11 -0400

    Friends pairing problem

--- src/App.java
+++ src/App.java
@@ -4,19 +4,12 @@ import javax.lang.model.util.Elements;
 public class App {
 
   public static void main(String[] args) {
-    int m = 3;
-    int n = 8;
-
+    int n = 5;
     int[] dp = new int[n + 1];
-
-    for (int i = 1; i <= n; i++) {
-      if (i < m) {
-        dp[i] = 1;
-      } else if (i == m) {
-        dp[i] = 2;
-      } else {
-        dp[i] = dp[i - 1] + dp[i - m];
-      }
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + (i - 1) * dp[n - 2];
     }
     System.out.println(dp[n]);
   }

b30a7688d6b8ab1ee225754c3c6f4b6ade50218f
Prabhat <krsinghp8@gmail.com>
2024-05-03 14:38:04 -0400

    Buy and sell stocks with transaction fee - Infinite transactions allowed

--- src/App.java
+++ src/App.java
@@ -5,26 +5,30 @@ public class App {
 
   public static void main(String[] args) {
     int[] arr = { 10, 15, 17, 20, 16, 18, 22, 20, 22, 20, 23, 25 };
-    int fee = 3;
-
     int obsp = -arr[0];
     int ossp = 0;
+    int ocsp = 0;
+
     for (int i = 1; i < arr.length; i++) {
-      int nbsp = 0;
-      int nssp = 0;
-      if (ossp - arr[i] > obsp) {
-        nbsp = ossp - arr[i];
+      int nbsp = 0, nssp = 0, ncsp = 0;
+      if (ocsp - arr[i] > obsp) {
+        nbsp = ocsp - arr[i];
       } else {
         nbsp = obsp;
       }
-
-      if (obsp + arr[i] - fee > ossp) {
-        nssp = obsp + arr[i] - fee;
+      if (obsp + arr[i] > ossp) {
+        nssp = obsp + arr[i];
       } else {
         nssp = ossp;
       }
+      if (ossp > ocsp) {
+        ncsp = ossp;
+      } else {
+        ncsp = ocsp;
+      }
       obsp = nbsp;
       ossp = nssp;
+      ocsp = ncsp;
     }
     System.out.println(ossp);
   }

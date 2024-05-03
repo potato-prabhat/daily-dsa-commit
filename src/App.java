6a3c9bef6da042b1a75b849ca4c0c7cd4e2e2ef1
Prabhat <krsinghp8@gmail.com>
2024-05-03 10:52:07 -0400

    partition into subsets

--- src/App.java
+++ src/App.java
@@ -5,12 +5,19 @@ public class App {
 
   public static void main(String[] args) {
     int n = 5;
-    int[] dp = new int[n + 1];
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; i++) {
-      dp[i] = dp[i - 1] + (i - 1) * dp[n - 2];
+    int k = 4;
+    int[][] dp = new int[k + 1][n + 1];
+    for (int t = 1; t <= k; t++) {
+      for (int p = 1; p <= n; p++) {
+        if (p < t) {
+          dp[t][p] = 0;
+        } else if (t == p) {
+          dp[t][p] = 1;
+        } else {
+          dp[t][p] = dp[t - 1][p - 1] + dp[t][p - 1] * t;
+        }
+      }
     }
-    System.out.println(dp[n]);
+    System.out.println(dp[k][n]);
   }
 }

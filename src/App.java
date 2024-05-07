0e6718219d0b515cc3fec8e0394f94883d95f81f
Prabhat <krsinghp8@gmail.com>
2024-05-07 01:16:49 -0400

    Q: Get connected components of a graph

--- src/App.java
+++ src/App.java
@@ -18,120 +18,42 @@ public class App {
 
   public static void main(String[] args) {
     int vtces = 7;
-    int[] v1 = { 0, 1, 2, 0, 3, 4, 5, 4 };
-    int[] v2 = { 1, 2, 3, 3, 4, 5, 6, 6 };
-    int[] wt = { 10, 10, 10, 40, 2, 3, 3, 8 };
+    int[] v1 = { 0, 2, 4, 5, 4 };
+    int[] v2 = { 1, 3, 5, 6, 6 };
+    int[] wt = { 10, 10, 10, 10, 10 };
     ArrayList<Edge>[] graph = new ArrayList[vtces];
     for (int i = 0; i < vtces; i++) {
       graph[i] = new ArrayList<>();
     }
-    int edges = 8;
+    int edges = 5;
     for (int i = 0; i < edges; i++) {
       graph[v1[i]].add(new Edge(v1[i], v2[i], wt[i]));
       graph[v2[i]].add(new Edge(v2[i], v1[i], wt[i]));
     }
-    int criteriaWt = 40;
-    int kthLargestPath = 3;
-    int src = 0;
-    int dest = 6;
+    ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
     boolean[] visited = new boolean[vtces];
-    multiSolver(
-      graph,
-      src,
-      dest,
-      visited,
-      criteriaWt,
-      kthLargestPath,
-      src + "",
-      0
-    );
-    System.out.println(spath + " " + spathwt);
-    System.out.println(lpath + " " + lpathwt);
-    System.out.println(cpath + " " + criteriaWt);
-    System.out.println(fpath + " " + fpathwt);
-    System.out.println(pq.peek().psf + " " + pq.peek().wsf);
+    for (int v = 0; v < vtces; v++) {
+      if (visited[v] == false) {
+        ArrayList<Integer> comp = new ArrayList<>();
+        drawTreeAndGenerateComp(graph, v, comp, visited);
+        comps.add(comp);
+      }
+    }
+    System.out.println(comps);
   }
 
-  static class Pair implements Comparable<Pair> {
-
-    int wsf;
-    String psf;
-
-    Pair(int wsf, String psf) {
-      this.psf = psf;
-      this.wsf = wsf;
-    }
-
-    public int compareTo(Pair o) {
-      return this.wsf - o.wsf;
-    }
-  }
-
-  static String spath;
-  static Integer spathwt = Integer.MAX_VALUE;
-
-  static String lpath;
-  static Integer lpathwt = Integer.MIN_VALUE;
-
-  static String cpath;
-  static Integer cpathwt = Integer.MAX_VALUE;
-
-  static String fpath;
-  static Integer fpathwt = Integer.MIN_VALUE;
-  static PriorityQueue<Pair> pq = new PriorityQueue<>();
-
-  public static void multiSolver(
+  public static void drawTreeAndGenerateComp(
     ArrayList<Edge>[] graph,
     int src,
-    int dest,
-    boolean[] visited,
-    int criteriaWt,
-    int kthLargestPath,
-    String psf,
-    int wsf
+    ArrayList<Integer> comp,
+    boolean[] visited
   ) {
-    if (src == dest) {
-      if (wsf < spathwt) {
-        spathwt = wsf;
-        spath = psf;
-      }
-      if (wsf > lpathwt) {
-        lpathwt = wsf;
-        lpath = psf;
-      }
-      if (wsf > criteriaWt && wsf < cpathwt) {
-        cpathwt = wsf;
-        cpath = psf;
-      }
-      if (wsf < criteriaWt && wsf > fpathwt) {
-        fpathwt = wsf;
-        fpath = psf;
-      }
-      if (pq.size() < kthLargestPath) {
-        pq.add(new Pair(wsf, psf));
-      } else {
-        if (wsf > pq.peek().wsf) {
-          pq.remove();
-          pq.add(new Pair(wsf, psf));
-        }
-      }
-      return;
-    }
     visited[src] = true;
+    comp.add(src);
     for (Edge e : graph[src]) {
       if (visited[e.nbr] == false) {
-        multiSolver(
-          graph,
-          e.nbr,
-          dest,
-          visited,
-          criteriaWt,
-          kthLargestPath,
-          psf + e.nbr,
-          wsf + e.wt
-        );
+        drawTreeAndGenerateComp(graph, e.nbr, comp, visited);
       }
     }
-    visited[src] = false;
   }
 }

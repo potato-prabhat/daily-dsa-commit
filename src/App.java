c4514e37d4b0745e07d03afef9f5a831be263925
Prabhat <krsinghp8@gmail.com>
2024-05-07 21:52:35 -0400

    hamiltonian paths and cycle

--- src/App.java
+++ src/App.java
@@ -1,53 +1,72 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.HashSet;
 
 public class App {
 
-  public static void main(String[] args) {
-    int[][] arr = {
-      { 0, 0, 1, 1, 1, 1, 1, 1 },
-      { 0, 0, 1, 1, 1, 1, 1, 1 },
-      { 1, 1, 1, 1, 1, 1, 1, 0 },
-      { 1, 1, 0, 0, 0, 1, 1, 0 },
-      { 1, 1, 1, 1, 0, 1, 1, 0 },
-      { 1, 1, 1, 1, 0, 1, 1, 0 },
-      { 1, 1, 1, 1, 1, 1, 1, 0 },
-      { 1, 1, 1, 1, 1, 1, 1, 0 },
-    };
-    boolean[][] visited = new boolean[arr.length][arr[0].length];
-    int count = 0;
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr[i].length; j++) {
-        if (arr[i][j] == 0 && visited[i][j] == false) {
-          drawTreeForComponent(arr, i, j, visited);
-          count++;
-        }
-      }
+  static class Edge {
+
+    int src;
+    int nbr;
+    int wt;
+
+    Edge(int src, int nbr, int wt) {
+      this.src = src;
+      this.nbr = nbr;
+      this.wt = wt;
     }
-    System.out.println(count);
   }
 
-  public static void drawTreeForComponent(
-    int[][] arr,
-    int i,
-    int j,
-    boolean[][] visited
-  ) {
-    if (
-      i < 0 ||
-      j < 0 ||
-      i >= arr.length ||
-      j >= arr[0].length ||
-      arr[i][j] == 1 ||
-      visited[i][j] == true
-    ) {
-      return;
+  public static void main(String[] args) {
+    int vtces = 7;
+    int[] v1 = { 0, 1, 2, 0, 3, 4, 5, 4 };
+    int[] v2 = { 1, 2, 3, 3, 4, 5, 6, 6 };
+    int[] wt = { 10, 10, 10, 10, 10, 10, 10, 10 };
+    ArrayList<Edge>[] graph = new ArrayList[vtces];
+    for (int i = 0; i < vtces; i++) {
+      graph[i] = new ArrayList<>();
+    }
+    int edges = 8;
+    for (int i = 0; i < edges; i++) {
+      graph[v1[i]].add(new Edge(v1[i], v2[i], wt[i]));
+      graph[v2[i]].add(new Edge(v2[i], v1[i], wt[i]));
     }
 
-    visited[i][j] = true;
-    drawTreeForComponent(arr, i - 1, j, visited);
-    drawTreeForComponent(arr, i, j + 1, visited);
-    drawTreeForComponent(arr, i, j - 1, visited);
-    drawTreeForComponent(arr, i + 1, j, visited);
+    int src = 0;
+
+    HashSet<Integer> visited = new HashSet<>();
+    int osrc = src;
+    hamiltonian(graph, src, visited, src + "", osrc);
+  }
+
+  public static void hamiltonian(
+    ArrayList<Edge>[] graph,
+    int src,
+    HashSet<Integer> visited,
+    String psf,
+    int osrc
+  ) {
+    if (visited.size() == graph.length - 1) {
+      System.out.print(psf);
+      boolean closingEdgeFound = false;
+      for (Edge e : graph[src]) {
+        if (e.nbr == osrc) {
+          closingEdgeFound = true;
+          break;
+        }
+      }
+      if (closingEdgeFound) {
+        System.out.println("*");
+      } else {
+        System.out.println(".");
+      }
+      return;
+    }
+    visited.add(src);
+    for (Edge e : graph[src]) {
+      if (visited.contains(e.nbr) == false) {
+        hamiltonian(graph, e.nbr, visited, psf + e.nbr, osrc);
+      }
+    }
+    visited.remove(src);
   }
 }
